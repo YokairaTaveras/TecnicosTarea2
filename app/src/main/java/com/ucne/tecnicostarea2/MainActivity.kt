3276520fc95fea5c581e5c6231d8def6d97a8a5a
YokairaTaveras <143297254+YokairaTaveras@users.noreply.github.com>
2024-05-23 17:07:59 -0400

    Agregando MainActivity

--- app/src/main/java/com/ucne/tecnicostarea2/MainActivity.kt
+++ app/src/main/java/com/ucne/tecnicostarea2/MainActivity.kt
@@ -1,38 +1,34 @@
 package com.ucne.tecnicostarea2
 
-import android.content.Context
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import androidx.room.Room
 import com.ucne.tecnicostarea2.data.local.database.TecnicoDb
-import com.ucne.tecnicostarea2.data.local.entities.TecnicoEntity
 import com.ucne.tecnicostarea2.data.local.repository.TecnicoRepository
+import com.ucne.tecnicostarea2.presentation.TecnicoScreen
 import com.ucne.tecnicostarea2.presentation.TecnicoListScreen
 import com.ucne.tecnicostarea2.presentation.TecnicoViewModel
-import com.ucne.tecnicostarea2.presentation.TicketScreen
 import com.ucne.tecnicostarea2.ui.theme.TecnicosTarea2Theme
+import kotlinx.serialization.Serializable
+import androidx.navigation.compose.composable
+import com.ucne.tecnicostarea2.data.local.repository.TipoTecnicoRepository
+import com.ucne.tecnicostarea2.presentation.Tipo.TipoTecnicoListScreen
+import com.ucne.tecnicostarea2.presentation.Tipo.TipoTecnicoScreen
+import com.ucne.tecnicostarea2.presentation.Tipo.TipoTecnicoViewModel
 
 class MainActivity : ComponentActivity() {
     private lateinit var tecnicoDb: TecnicoDb
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         tecnicoDb = Room.databaseBuilder(
-            applicationContext,
+            this,
             TecnicoDb::class.java,
             "Tecnico.db"
         )
@@ -40,55 +36,81 @@ class MainActivity : ComponentActivity() {
             .build()
 
         val repository = TecnicoRepository(tecnicoDb.tecnicoDao())
-
-        val viewModel = ViewModelProvider(
-            this,
-            TecnicoViewModel.provideFactory(applicationContext, repository)
-        ).get(TecnicoViewModel::class.java)
-
+        val repository2 = TipoTecnicoRepository(tecnicoDb.tipoTecnicoDao())
         enableEdgeToEdge()
         setContent {
             TecnicosTarea2Theme {
-                Surface {
-                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                        Column(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(innerPadding)
-                                .padding(8.dp)
-                        ) {
-                            TicketScreen(viewModel = viewModel)
-                            TecnicoListScreen(
-                                viewModel = viewModel,
-                                onVerTecnico = {  },
-                                onEliminarTecnico = { tecnico ->
-                                    viewModel.deleteTecnico(tecnico)
-                                    showNotification("Técnico eliminado")
-                                },
-                                onGuardarTecnico = { tecnico ->
-                                    if (viewModel.saveTecnico(tecnico)) {
-                                        showNotification("Técnico guardado")
-                                    } else {
-                                        showNotification("Nombre de técnico duplicado o campos incompletos")
-                                    }
-                                }
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = Screen.TecnicoList) {
+
+                    composable<Screen.TecnicoList> {
+                        TecnicoListScreen(
+                            viewModel = viewModel { TecnicoViewModel(repository, 0) },
+                            onVerTecnico = {
+                                navController.navigate(Screen.Tecnico(it.tecnicoId ?: 0))
+                            },
+                            onEliminarTecnico = {},
+                            onGuardarTecnico = { },
+                            navController = navController,
+                            onAddTecnico = {}
+
                             )
-                        }
+
+                    }
+                    composable<Screen.Tecnico> {
+                        val args = it.toRoute<Screen.Tecnico>()
+                        TecnicoScreen(
+                            viewModel = viewModel { TecnicoViewModel(repository, args.tecnicoId) },
+                            navController = navController
+                        )
+                    }
+                    composable<Screen.TipoTecnico> {
+                        val args = it.toRoute<Screen.TipoTecnico>()
+                        TipoTecnicoScreen(viewModel = viewModel {
+                            TipoTecnicoViewModel(
+                                repository2,
+                                args.tipoTecnicoId
+                            )
+                        },
+                            navController = navController)
+                    }
+
+                    composable<Screen.TipoTecnicoList> {
+                        TecnicoListScreen(
+                            viewModel = viewModel { TecnicoViewModel(repository, 0) },
+                            onVerTecnico = {navController.navigate(Screen.Tecnico(it.tecnicoId ?: 0))} ,
+                            onGuardarTecnico = {navController.navigate(Screen.Tecnico(0))},
+                            navController = navController,
+                            onEliminarTecnico ={},
+                            onAddTecnico = {},
+                            )
+
                     }
                 }
             }
         }
     }
-
-    private fun showNotification(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
 }
 
-@Preview(showBackground = true)
+sealed class Screen {
+    @Serializable
+    object TecnicoList : Screen()
+
+    @Serializable
+    data class Tecnico(val tecnicoId: Int) : Screen()
+
+    @Serializable
+    object TipoTecnicoList : Screen()
+
+    @Serializable
+    data class TipoTecnico(val tipoTecnicoId: Int) : Screen()
+}
+
+
+/*@Preview(showBackground = true)
 @Composable
 fun Preview() {
     TecnicosTarea2Theme {
 
     }
-}
+}*/
