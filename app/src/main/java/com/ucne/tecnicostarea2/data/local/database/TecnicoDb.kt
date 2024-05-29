c2f90ce296e7c6a2b093fe5190ebaa4c443035db
YokairaTaveras <143297254+YokairaTaveras@users.noreply.github.com>
2024-05-28 22:05:46 -0400

    Agregando servicio

--- app/src/main/java/com/ucne/tecnicostarea2/data/local/database/TecnicoDb.kt
+++ app/src/main/java/com/ucne/tecnicostarea2/data/local/database/TecnicoDb.kt
@@ -2,20 +2,24 @@ package com.ucne.tecnicostarea2.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.ucne.tecnicostarea2.data.local.dao.ServicioDao
 import com.ucne.tecnicostarea2.data.local.dao.TecnicoDao
 import com.ucne.tecnicostarea2.data.local.dao.TipoTecnicoDao
+import com.ucne.tecnicostarea2.data.local.entities.ServicioEntity
 import com.ucne.tecnicostarea2.data.local.entities.TecnicoEntity
 import com.ucne.tecnicostarea2.data.local.entities.TipoTecnicoEntity
 
 @Database(
     entities = [
         TecnicoEntity::class,
-        TipoTecnicoEntity::class
+        TipoTecnicoEntity::class,
+        ServicioEntity::class
     ],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 abstract class TecnicoDb: RoomDatabase() {
     abstract fun tecnicoDao(): TecnicoDao
     abstract fun tipoTecnicoDao(): TipoTecnicoDao
+    abstract fun servicioDao(): ServicioDao
 }
